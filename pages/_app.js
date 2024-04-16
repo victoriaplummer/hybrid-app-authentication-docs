0aaaedeea9a16dad07c9715e7ed738a77eeb7e34
victoriaplummer <victoria.l.plummer@gmail.com>
2024-04-16 11:56:27 -0400

    Styled Code to copy

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,20 @@ import Head from "next/head"
 import "../global.css"
 import "../custom-ch.css"
 
+const copyCode = () => {
+  const codeBlock = document.getElementById('code-block');
+  const text = codeBlock.textContent.trim(); // Get the text content without leading/trailing spaces
+
+  // Remove leading $ symbol if present
+  const textWithoutDollar = text.startsWith('$ ') ? text.substring(2) : text;
+
+  // Copy text to clipboard
+  navigator.clipboard.writeText(textWithoutDollar)
+
+}
+
+export {copyCode}
+
 function MyApp({ Component, pageProps }) {
   return (
     <div>
